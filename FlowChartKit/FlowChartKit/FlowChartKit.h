77838f1a1fb32ff571033090babdad492a5f8d92
Kay Old <kaykwing@gmail.com>
2022-01-16 21:41:47 +0800

    add project init

---
+++ FlowChartKit/FlowChartKit/FlowChartKit.h
@@ -0,0 +1,18 @@
+//
+//  FlowChartKit.h
+//  FlowChartKit
+//
+//  Created by Kay Chang on 2022/1/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlowChartKit.
+FOUNDATION_EXPORT double FlowChartKitVersionNumber;
+
+//! Project version string for FlowChartKit.
+FOUNDATION_EXPORT const unsigned char FlowChartKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlowChartKit/PublicHeader.h>
+
+
